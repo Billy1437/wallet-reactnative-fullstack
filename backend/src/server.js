1a490ab3663e0c9a4e37eefc2915d3a9b5d30c8b
Min Phone Thit <billy@Mins-MacBook-Air.local>
2025-12-14 15:10:09 +0630

    updated cron

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,16 +5,24 @@ import ratelimiter from "./middleware/ratelimit.js";
 import { initDb } from "./config/db.js";
 
 import transactionsRoute from "./routes/transactionsRoute.js";
-
+import job from "./config/cron.js";
 dotenv.config();
 
 const app = express();
+
+if(process.env.NODE_ENV === "production") job.start()
+
+
 
 //middleware
 app.use(ratelimiter);
 app.use(express.json());
 
 const PORT = process.env.PORT;
+
+app.get("/api/health",(req,res) => {
+  res.status(200).json({status : "ok"})
+})
 
 app.use("/api/transactions", transactionsRoute);
 
